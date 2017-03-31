b4ce6ce8df28970c734ec9eb1bf858f2268fe505
Emiliya <vuntsova@aol.com>
2017-03-31 19:03:28 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -3,6 +3,6 @@ console.log('this is loaded');
 exports.twitterKeys = {
   consumer_key: 'oJ1u3LkDj7z90GTKzcoJtaTuK',
   consumer_secret: 'rU3SaZoGEXMVNnYp1G1OH232etJwGmcHarCQ0oS1d6fPpwMAs7',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  access_token_key: '847941773708189701-awwRzhDz8L4KASW4DN9MFTkJANPpf5I',
+  access_token_secret: 'ULMra8vaV1Agon88jANIYjxuOUdPBDvbQ8hQZ2sPTc31T',
 }
