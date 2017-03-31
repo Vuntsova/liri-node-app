11666a81764d39a1cff8672e778dc0c88f57e281
Emiliya <vuntsova@aol.com>
2017-03-31 19:01:49 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
+  consumer_key: 'oJ1u3LkDj7z90GTKzcoJtaTuK',
+  consumer_secret: 'rU3SaZoGEXMVNnYp1G1OH232etJwGmcHarCQ0oS1d6fPpwMAs7',
   access_token_key: '<input here>',
   access_token_secret: '<input here>',
 }
