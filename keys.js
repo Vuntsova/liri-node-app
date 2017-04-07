b3290538a17858cf4640ab369054825275e8397d
vuntsova <vuntsova@aol.com>
2017-04-07 11:54:33 -0400

    1

--- keys.js
+++ keys.js
@@ -1,8 +1,9 @@
 console.log('this is loaded');
 
+var exports;
 exports.twitterKeys = {
   consumer_key: 'oJ1u3LkDj7z90GTKzcoJtaTuK',
   consumer_secret: 'rU3SaZoGEXMVNnYp1G1OH232etJwGmcHarCQ0oS1d6fPpwMAs7',
   access_token_key: '847941773708189701-awwRzhDz8L4KASW4DN9MFTkJANPpf5I',
-  access_token_secret: 'ULMra8vaV1Agon88jANIYjxuOUdPBDvbQ8hQZ2sPTc31T',
-}
+  access_token_secret: 'ULMra8vaV1Agon88jANIYjxuOUdPBDvbQ8hQZ2sPTc31T'
+};
--- liri.js
+++ liri.js
@@ -1,1 +1,54 @@
+//Declare Variables
+var keys = require("./keys.js");
+console.log(keys.twitterKeys);
+var Twitter = require('twitter');
+var command = process.argv[2];
+var values = process.argv[3];
 
+
+//Find out what your commands are
+switch(command) {
+    case "my-tweets":
+        tweetsFunction();
+        break;
+    case "spotify-this-song":
+        spotifyFunction();
+        break;
+    case "movie-this":
+        movieFunction();
+        break;
+    case "do-what-it-says":
+        sayFunction();
+        break;
+    default: console.log("Command Not Found!")
+}
+//Function for getting Tweets
+function tweetsFunction() {
+    var client = new Twitter(keys.twitterKeys);
+    var params = {screen_name: 'nyt',
+                    count:21
+                 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if(error){
+            console.log(error);
+        }else if (!error) {
+
+            console.log(tweets[10].text);
+        }
+    });
+ console.log("Here is a tweet")
+};
+//Function for getting Spotify
+function spotifyFunction() {
+    console.log("Here is spotify")
+};
+//Function for getting Movies
+function movieFunction() {
+    console.log("Here is a movie")
+
+};
+//Function for getting Things
+function sayFunction() {
+    console.log("Here is something")
+
+};
